d647f69ec29e46242c70387e25ba48de66abde35
Steve Hunley <shunley2000@yahoo.com>
2022-11-02 08:26:57 -0500

    Get column widths working

--- .erb/scripts/jest-shim.js
+++ .erb/scripts/jest-shim.js
@@ -1,3 +1,4 @@
+// mosesoak @ https://stackoverflow.com/questions/57712235/referenceerror-textencoder-is-not-defined-when-running-react-scripts-test
 import { TextEncoder } from 'util'
 
 global.TextEncoder = TextEncoder
--- src/renderer/components/CardList/CardList.tsx
+++ src/renderer/components/CardList/CardList.tsx
@@ -9,26 +9,59 @@ const columnDefs = [
   {
     field: 'question',
     sortable: true,
-    filter: true
+    filter: true,
+    flex: 2
   },
   {
     field: 'answer',
     sortable: true,
-    filter: true
+    filter: true,
+    flex: 2
   },
   {
     field: 'score',
     sortable: true,
-    filter: true
+    filter: true,
+    flex: 1,
+    comparator: (valueA: string, valueB: string) => {
+      // valueA and valueB are in format <num>% or for ex 100%
+      // Strip off the '%' to get the num
+      const numA = Number(valueA.slice(0, -1))
+      const numB = Number(valueB.slice(0, -1))
+
+      if (numA === numB) {
+        return 0
+      }
+      if (numA > numB) {
+        return 1
+      }
+      return -1
+    }
   }
 ]
+
+const handleSortChanged = () => {
+  const divs = document.querySelectorAll('.ag-root .ag-center-cols-container')
+
+  if (!divs || divs.length < 0) return
+
+  const gridDiv = divs[0]
+
+  if (!gridDiv) return
+
+  gridDiv.scrollIntoView(true)
+}
 
 const CardList = () => {
   const { cueCards } = useAppSelector((state) => state.cueCards)
 
   return (
     <div className="ag-theme-alpine" style={{ height: 500 }}>
-      <AgGridReact rowData={cueCards} columnDefs={columnDefs} />
+      <AgGridReact
+        rowData={cueCards}
+        columnDefs={columnDefs}
+        onSortChanged={handleSortChanged}
+      />
     </div>
   )
 }
